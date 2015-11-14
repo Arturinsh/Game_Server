b5ab04bababbcb8d1f6ace8e4fed2ef3ede014df
Arturs Ontuzans <arturs.ontuzans@gmail.com>
2015-11-14 19:27:56 +0200

    Configured hibernate, added basic User class

---
+++ src/main/java/xyz/arturinsh/database/User.java
@@ -0,0 +1,44 @@
+package xyz.arturinsh.database;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Users")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long Id;
+	
+	private String Username;
+	
+	private String Password;
+
+	public long getId() {
+		return Id;
+	}
+
+	public void setId(long id) {
+		Id = id;
+	}
+
+	public String getUsername() {
+		return Username;
+	}
+
+	public void setUsername(String username) {
+		Username = username;
+	}
+
+	public String getPassword() {
+		return Password;
+	}
+
+	public void setPassword(String password) {
+		Password = password;
+	}
+}
--- src/main/java/xyz/arturinsh/gameserver/Main.java
+++ src/main/java/xyz/arturinsh/gameserver/Main.java
@@ -1,7 +1,45 @@
 package xyz.arturinsh.gameserver;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.hibernate.Session;
+
+import com.esotericsoftware.kryonet.Server;
+
+import xyz.arturinsh.database.User;
+import xyz.arturinsh.helpers.SessionFactoryUtil;
+
 public class Main {
 	public static void main(String args[]) {
+		//testKryo();
 		System.out.println("Hello World");
+//		testHibernate();
 	}
+
+	private static void testKryo() {
+		Server server = new Server();
+	    server.start();
+	    try {
+			server.bind(54555, 54777);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private static void testHibernate() {
+		Session session = SessionFactoryUtil.getSessionFactory().openSession();
+		User test = new User();
+		test.setPassword("t");
+		test.setUsername("kaka");
+		
+		session.beginTransaction();
+		
+		session.save(test);
+		
+		session.getTransaction().commit();
+		session.close();
+	}
+
 }

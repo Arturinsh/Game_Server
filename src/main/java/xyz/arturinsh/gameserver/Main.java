d76de47aba1b756227e2559c3b9d65e922dffb44
Arturs Ontuzans <arturs.ontuzans@gmail.com>
2015-11-14 20:36:44 +0200

    Added c3p0 pool for hibernate, configured loggers
    Loggers set to WARNING level

--- src/main/java/xyz/arturinsh/gameserver/Main.java
+++ src/main/java/xyz/arturinsh/gameserver/Main.java
@@ -1,7 +1,8 @@
 package xyz.arturinsh.gameserver;
 
-import java.io.File;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.hibernate.Session;
 
@@ -12,15 +13,16 @@ import xyz.arturinsh.helpers.SessionFactoryUtil;
 
 public class Main {
 	public static void main(String args[]) {
-		//testKryo();
-		System.out.println("Hello World");
-//		testHibernate();
+		System.out.println("Server started");
+		setLoggersToLogWarining();
+		testHibernate();
+		System.out.println("The end");
 	}
 
 	private static void testKryo() {
 		Server server = new Server();
-	    server.start();
-	    try {
+		server.start();
+		try {
 			server.bind(54555, 54777);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
@@ -28,16 +30,23 @@ public class Main {
 		}
 	}
 
+	private static void setLoggersToLogWarining() {
+		Logger log = Logger.getLogger("org.hibernate");
+		log.setLevel(Level.WARNING);
+		System.setProperty("com.mchange.v2.log.FallbackMLog.DEFAULT_CUTOFF_LEVEL", "WARNING");
+		System.setProperty("com.mchange.v2.log.MLog", "com.mchange.v2.log.FallbackMLog");
+	}
+
 	private static void testHibernate() {
 		Session session = SessionFactoryUtil.getSessionFactory().openSession();
 		User test = new User();
-		test.setPassword("t");
+		test.setPassword("testdsdf");
 		test.setUsername("kaka");
-		
+
 		session.beginTransaction();
-		
+
 		session.save(test);
-		
+
 		session.getTransaction().commit();
 		session.close();
 	}

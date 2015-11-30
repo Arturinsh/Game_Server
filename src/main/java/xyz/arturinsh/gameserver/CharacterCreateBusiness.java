19c143a674e50b287210102ce650c23d9cfc0226
Arturs Ontuzans <arturs.ontuzans@gmail.com>
2015-11-30 23:30:31 +0200

    Now cant save characters with same names

--- src/main/java/xyz/arturinsh/gameserver/CharacterCreateBusiness.java
+++ src/main/java/xyz/arturinsh/gameserver/CharacterCreateBusiness.java
@@ -1,5 +1,8 @@
 package xyz.arturinsh.gameserver;
 
+import java.util.List;
+
+import org.hibernate.Query;
 import org.hibernate.Session;
 
 import com.esotericsoftware.kryonet.Server;
@@ -9,6 +12,8 @@ import xyz.arturinsh.database.User;
 import xyz.arturinsh.gameObjects.CharacterClass;
 import xyz.arturinsh.gameserver.Main.PlayerConnection;
 import xyz.arturinsh.helpers.SessionFactoryUtil;
+import xyz.arturinsh.packets.Packets.CharacterCreateFailed;
+import xyz.arturinsh.packets.Packets.CharacterCreateSuccess;
 import xyz.arturinsh.packets.Packets.UserCharacter;
 
 public class CharacterCreateBusiness {
@@ -20,17 +25,24 @@ public class CharacterCreateBusiness {
 
 	public void createCharacter(PlayerConnection playerConnection, UserCharacter newChar) {
 		String charName = newChar.charName;
-		CharacterClass charClass = newChar.charClass;
-		GameCharacter ch = new GameCharacter();
-		ch.setCharacterName(charName);
-		ch.setCharClass(charClass);
-		User user = playerConnection.user;
-		ch.setUser(user);
-		
-		Session session = SessionFactoryUtil.getSessionFactory().openSession();
-		session.beginTransaction();
 
-		session.save(ch);
-		session.getTransaction().commit();
+		Session session = SessionFactoryUtil.getSessionFactory().openSession();
+		Query query = session.createQuery("From GameCharacter Where CharName =:name");
+		query.setParameter("name", charName);
+		List<GameCharacter> characters = query.list();
+		if (characters.size() < 1) {
+			CharacterClass charClass = newChar.charClass;
+			GameCharacter ch = new GameCharacter();
+			ch.setCharacterName(charName);
+			ch.setCharClass(charClass);
+			User user = playerConnection.user;
+			ch.setUser(user);
+			session.beginTransaction();
+			session.save(ch);
+			session.getTransaction().commit();
+			playerConnection.sendTCP(new CharacterCreateSuccess());
+		} else
+			playerConnection.sendTCP(new CharacterCreateFailed());
+
 	}
 }
--- src/main/java/xyz/arturinsh/packets/Packets.java
+++ src/main/java/xyz/arturinsh/packets/Packets.java
@@ -46,5 +46,6 @@ public class Packets {
 	}
 
 	public static class CharacterCreateFailed {
+		
 	}
 }
